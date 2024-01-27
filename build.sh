cb290ba2dfafa9db5895dc8c512b18454b35c30d
Georges Savoundararadj <savoundg@gmail.com>
2024-01-27 00:50:40 -0800

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+ALPINE_BUILDER_VERSION="3.19.1"
+USER="manoj23"
+REPO="file_backup"
+DOCKERFILE_HASH=$(git rev-parse --short HEAD)
+BUILDER="alpine-${ALPINE_BUILDER_VERSION}"
+
+docker_build_tag_and_push()
+{
+	IMAGE="$1"
+	TAG="${IMAGE}:${BUILDER}"
+
+	docker build "https://github.com:/${USER}/dockerfile-${REPO}.git" \
+		--build-arg "ALPINE_VERSION=${ALPINE_BUILDER_VERSION}" \
+		--build-arg "DOCKERFILE_HASH=${DOCKERFILE_HASH}" \
+		-t "$TAG"
+
+	docker tag "${TAG}" "ghcr.io/${USER}/${TAG}"
+	docker push "ghcr.io/${USER}/${TAG}"
+}
+
+docker_build_tag_and_push "file_backup"
